b42624545526693f704468c9342777ea90f2f390
boris <borborich@bk.ru>
2024-03-11 12:15:09 +0100

    Кошельки

---
+++ donate.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="ru"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Поблагодарить Шестаковых</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Добавленные кнопки -->
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col text-left">
+                <a href="index.php" class="btn btn-secondary">Главная страница</a>
+            </div>
+            <div class="col text-center">
+                <a href="table.php" class="btn btn-secondary">Перейти к таблицам</a>
+            </div>
+            <div class="col text-right">
+                <a href="cards.php" class="btn btn-secondary">Перейти к карточкам</a>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-2">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h2><strong>Поддержать проект 👍</strong></h2>
+                        Научно доказано что если обучение оплачено, то знания усваиваются в 5 раз быстрее! 😉<br>
+                       <br>
+                       <strong>Дальнейшие цели:</strong>
+                       <ul>
+                           <li>📚 Добавить другие языки для изучения</li>
+                           <li>🪪 Сделать персональные аккаунты с возможностью создавать свои таблицы</li>
+                           <li>🔊 Аудио произношение слов 🗣️</li>
+                           <li>🔥 Ваши варианты от 200€ 😉</li>
+                       </ul>
+                       <br>
+                       Но вы же понимаете, жена художница 👩‍🎨, трое детей 🧒🧒👶, управление B&B 🛋️, стройка 🇮🇹 👷, развитие винного стартапа 🍷💼. То что эти карточки появились – это уже подвиг 😂, поэтому для развития нужна небольшая ваша благодарность 💶. <br> <strong>Спасибо 🤗 от чистого сердца ❤️ из Италии 🇮🇹</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h2><strong>На карту Тиньков (карты РФ)</strong></h2>
+                        Если вы из России, перевод на карту будет проще всего. По клику откроется удобное окошко.<br>
+                       <small> Либо по номеру карты: 5536914011005836 </small><br><br>
+                        <a href="https://www.tinkoff.ru/rm/shestakov.boris8/u4fIy79873" class="btn btn-primary btn-lg float-right" target="_blank">
+                            💳 Донатить
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h2><strong>Через Stripe (карты не РФ)</strong></h2>
+                        Позволяет напрямую задонатить мне в Евро с любых зарубежных карт. Поддерживает Google и Apple Pay.<br>
+                       <br>
+                        <a href="https://donate.stripe.com/00g7vB2Ty8Vs6jK000" class="btn btn-primary btn-lg float-right" target="_blank">
+                            💶 Донатить
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h2><strong>PayPal</strong></h2>
+                        Вариант для живущих заграницей и просто у кого там завалялась мелочь на аккаунте.<br>
+                       <br>
+                        <a href="https://paypal.me/shestakovboris" class="btn btn-primary btn-lg float-right" target="_blank">
+                            💰 PayPal.me
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h2><strong>Крипта</strong></h2>
+                        <div class="row">
+                            <div class="col-md-4 crypto">
+                                <p><strong>Биткоин</strong></p>
+                                <img src="img/crypto_btc.png" alt="Bitcoin" class="img-fluid">
+                                <p>1GnT9Kncxt1dUCViWnkzZLghH951vRDhFQ</p>
+                                <small>Отправляйте только Bitcoin (ВТС) на этот адрес.</small><br><br>
+                            </div>
+                            <div class="col-md-4 crypto">
+                                <p><strong>Toncoin (TON)</strong></p>
+                                <img src="img/crypto_ton.png" alt="Toncoin (TON)" class="img-fluid">
+                                <p>UQAWCHA6edsw2G1pfX2idY4dD_cN23VhwNrjdV9OhY4Dw4Or</p>
+                                <small>Отправляйте только Toncoin (TON) на этот адрес.</small><br><br>
+                            </div>
+                            <div class="col-md-4 crypto">
+                                <p><strong>USDT</strong></p>
+                                <img src="img/crypto_usdt.png" alt="USDT" class="img-fluid">
+                                <p>TEnkSutUvXfgBSBTnPJfu7yrDRdJSgjALW</p>
+                                <small>Отправляйте только USDT TRC20 на этот адрес. </small><br><br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body></html>
